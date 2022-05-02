22a2bee08cff46f14ed89acabb56930840e7a92b
Zichen Yang <easonyzc96@gmail.com>
2022-05-02 18:00:05 -0400

    try to solve cors

--- src/main/java/com/wow/rent/controller/AccountController.java
+++ src/main/java/com/wow/rent/controller/AccountController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import com.wow.rent.model.Result;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.xml.ws.RequestWrapper;
 
@@ -132,8 +133,9 @@ public class AccountController {
         }
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "null"})
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public Object login(@RequestBody LoginRequestEntry loginRequest, HttpServletRequest httpRequest) {
+    public Object login(@RequestBody LoginRequestEntry loginRequest, HttpServletRequest httpRequest, HttpServletResponse response) {
         Result<AccountEntry> result;
 
         result = accountServie.login(loginRequest);
@@ -141,19 +143,27 @@ public class AccountController {
         if (result.isSuccess()) {
             httpRequest.getSession().setAttribute(SESSION_NAME, result.getData());
         }
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Origin",request.getHeader("Origin"));
         return result;
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "null"})
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
-    public Object logout(HttpServletRequest request) {
+    public Object logout(HttpServletRequest request,HttpServletResponse response) {
         Result result = new Result();
         request.getSession().setAttribute(SESSION_NAME, null);
         result.setResultSuccess("Successfully logged out！", null);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Origin",request.getHeader("Origin"));
         return result;
     }
 
+    @CrossOrigin(origins = {"http://localhost:3000", "null"})
     @RequestMapping(value = "/islogin", method = RequestMethod.GET)
-    public Result<AccountEntry> isLogin(HttpServletRequest request) {
+    public Result<AccountEntry> isLogin(HttpServletRequest request,HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Origin",request.getHeader("Origin"));
         HttpSession session = request.getSession();
 
         System.out.println(request.getSession().getId());
